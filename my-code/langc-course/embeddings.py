11732bbe7b4fdb54a703884e8799ed9d5cd1410f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2026-07-21 02:44:40 -0300

    Hands-on: OpenAI Embedding

---
+++ my-code/langc-course/embeddings.py
@@ -0,0 +1,30 @@
+# Model                Dimensions    Cost per 1M tokens    Best For
+# text-embedding-3-small    1536          $0.02             General use
+# text-embedding-3-large    3072          $0.13             High accuracy
+# text-embedding-ada-002    1536          $0.10             Legacy
+
+from langchain_openai.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# Single text query / document
+embedding = embeddings.embed_query("This is a test document.")
+# print(f"Embedding for single text: {embedding}")
+
+print(len(embedding))  # Should print 1536 for text-embedding-3-small
+
+
+# multiple texts
+embeds = embeddings.embed_documents(
+    [
+        "This is the first document.",
+        "This is the second document."
+    ]
+)
+
+print(f"Embeddings for multiple texts: {embeds}")
+print(f"Number of embeddings returned: {len(embeds)}")  # Should print 2
+print(f"Length of each embedding: {len(embeds[0])}")  # Should print 1536 for text-embedding-3-small
