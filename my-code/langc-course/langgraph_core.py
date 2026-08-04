c54f37d3551b8496ae6a31edaa44e08d2e68c1f9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2026-08-03 21:42:27 -0300

    Hands-on: Create Your First Simple Graph

---
+++ my-code/langc-course/langgraph_core.py
@@ -0,0 +1,61 @@
+"""
+LangGraph Core Concepts
+StateGraph, nodes, edges, and basic patterns
+"""
+
+from langchain.chat_models import init_chat_model
+from langgraph.graph import StateGraph, START, END
+from typing_extensions import TypedDict, Annotated
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+import operator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Basic state
+class SimpleState(TypedDict):
+    input: str
+    output: str
+    step: int
+
+
+def demo_simple_graph():
+    # define node functions
+    def process(state: SimpleState) -> dict:
+        # simple processing logic, for demo purposes
+        return {"output": state["input"].upper(), "step": state["step"] + 1}
+
+    # create graph
+    graph = StateGraph(SimpleState)
+
+    # add nodes
+    graph.add_node("process", process)
+    # add edges
+    graph.add_edge(START, "process")
+    graph.add_edge("process", END)
+
+    # execute graph/ compile
+    app = graph.compile()
+
+    # # visualize the graph
+    # print("\n--- Mermaid Graph ---")
+    # print(app.get_graph().draw_mermaid())
+
+    # # save as PNG
+    # png_bytes = app.get_graph().draw_mermaid_png()
+    # with open("graph.png", "wb") as f:
+    #     f.write(png_bytes)
+    # print("\nGraph saved to graph.png")
+
+    # run app
+    result = app.invoke({"input": "hello", "output": "", "step": 0})
+
+    print("simple graph result:", result)
+    print(
+        f" Input: {result['input']}, Output: {result['output']}, Step: {result['step']}"
+    )
+
+if __name__ == "__main__":
+    demo_simple_graph()
